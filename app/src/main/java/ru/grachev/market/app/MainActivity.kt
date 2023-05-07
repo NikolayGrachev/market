664f66c79742997571cd91d54f95552f1df43b67
Nikolay Grachev <nick@cinemood.com>
2023-05-07 23:12:53 +0200

    some fixes and plugin upgrade

--- app/src/main/java/ru/grachev/market/app/MainActivity.kt
+++ app/src/main/java/ru/grachev/market/app/MainActivity.kt
@@ -1,13 +1,7 @@
 package ru.grachev.market.app
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import ru.grachev.market.core_navigation_impl.R
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        // test1
-        // test2
-
-        // test3
-        // test4
-        val j = CoroutineScope(Job()).launch {  }
-
     }
 }
